e22eb3d5f8844234bcbec18c95fa840074ca5025
TomMuirhead <60030844+TomMuirhead@users.noreply.github.com>
2020-11-12 20:00:47 +0000

    Validating input

---
+++ Validate_Input.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Validate_Input : MonoBehaviour
+{
+    InputField inputField;
+    Color invalidColor;
+    Text input;
+    Image image;
+    public float inputSize;
+
+    public void OnValueChanged()
+    {
+        inputField = GetComponent<InputField>();
+        invalidColor = new Color(1f, 0f, 0f, 0.5f);
+        input = GetComponent<Text>();
+        image = GetComponent<Image>();
+
+        while (input != null)
+        {
+            bool valid = false;
+            while (valid == false)
+            {
+                image.color = invalidColor;
+            }
+        }
+    }
+}
