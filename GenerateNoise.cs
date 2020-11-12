f6c1b3d7be9c0fae91bad49d1b8d15eae9201684
TomMuirhead <60030844+TomMuirhead@users.noreply.github.com>
2020-11-12 20:05:28 +0000

    Perlin Noise and Assign Tile
    Perlin noise function to generate value for each tile from seed
    Tile attributes set in sub classes

---
+++ GenerateNoise.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateNoise : MonoBehaviour
+{
+    System.Random random = new System.Random();
+    private int seed; //offset the noise, Perlin Noise will always produce the same noise
+    float[] pNoiseArray;
+    public float scale = 1.0f; //Changes the size of waves of noise
+    private int width;
+    private int height;
+
+    void Start()
+    {
+        seed = random.Next();
+        width = GameObject.Find("Tilemap").GetComponent<SetMapBase>().mapArea.size.x;
+        height = GameObject.Find("Tilemap").GetComponent<SetMapBase>().mapArea.size.y;
+
+        pNoiseArray = new float[width * height]; //pNosieArray same size as the mapArray
+    }
+
+    void CalcNoise() //Use Perlin Noise to determine spread of the tiles
+    {
+        for (float i = 0f; i < height; i++) //Loop through Perlin Noise algorithm to fill the array
+        {
+            for (float j = 0f; j < width; j++)
+            {
+                float x = j + seed / width * scale;
+                float y = i + seed / height * scale;
+                float pNoise = Mathf.PerlinNoise(x, y); //Perlin Noise Function, pass in x and y coord
+                pNoiseArray[(int)i * height + (int)j] = pNoise; //2d map to 1d array
+            }
+        }
+    }
+}
---
+++ TileType.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Tile : TileBase
+{
+    private bool canTraverse;
+    private int resource;
+    private int defence;
+    private int baseConnection;
+
+    public Tile()
+    {
+        canTraverse = true;
+        resource = 0;
+        defence = 0;
+        baseConnection = 0;
+    }
+
+    public class DeepWater : Tile
+    {
+        public DeepWater() : base()
+        {
+            canTraverse = false;
+            resource = 10;
+            defence = 20;
+        }
+    }
+    public class ShallowWater :Tile
+    {
+        public ShallowWater() : base()
+        {
+            canTraverse = false;
+            resource = 20;
+            defence = 5;
+        }
+    }
+    public class Beach : Tile
+    {
+        public Beach() : base()
+        {
+            resource = 10;
+        }
+    }
+    public class Plain : Tile
+    {
+        public Plain() : base()
+        {
+            resource = 15;
+            defence = 5;
+        }
+    }
+    public class Forest : Tile
+    {
+        public Forest() : base()
+        {
+            resource = 10;
+            defence = 10;
+        }
+    }
+    public class Hills : Tile
+    {
+        public Hills() : base()
+        {
+            resource = 5;
+            defence = 20;
+        }
+    }
+    public class Mountain : Tile
+    {
+        public Mountain() : base()
+        {
+            canTraverse = false;
+            resource = 5;
+            defence = 30;
+        }
+    }
+    public class Snow : Tile
+    {
+        public Snow() : base()
+        {
+            canTraverse = false;
+            defence = 10;
+        }
+    }
+}
+/*
+public class TileType : MonoBehaviour
+{
+    public TileBase
+    baseTile, deepWater, shallowWater, beach, plain, forest, hills, mountain, snow;
+
+    
+    public void AssignTile(float[] pNoiseArray)
+    {
+        for (int i = 0; i < pNoiseArray.Length; i++) //Loop through tilemap array
+        {
+            switch (pNoiseArray[i]) //Assign tile type according to Perlin Noise value
+            {
+                case float n when n >= 0 && n < 0.125: //Perlin Noise gives value between 0 and 1
+                    setDeepWater(i); //Calling function to change tile, passing tile index
+                    break;
+                case float n when n >= 0.125 && n < 0.25:
+                    setShallowWater(i);
+                    break;
+                case float n when n >= 0.25 && n < 0.375:
+                    setBeach(i);
+                    break;
+                case float n when n >= 0.375 && n < 0.5:
+                    setPlain(i);
+                    break;
+                case float n when n >= 0.5 && n < 0.625:
+                    setForest(i);
+                    break;
+                case float n when n >= 0.625 && n < 0.75:
+                    setHills(i);
+                    break;
+                case float n when n >= 0.75 && n < 0.875:
+                    setMountain(i);
+                    break;
+                case float n when n >= 0.875 && n <= 1:
+                    setSnow(i);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void setDeepWater(int i) //Set tile to Deep Water Tile
+    {
+        deepWater = new Tile.DeepWater(); //Instantiate Deep Water Tile
+        mapArray[i] = deepWater; //Set tile at index i to Deep Water tile
+    }
+    private void setShallowWater(int i)
+    {
+        shallowWater = new Tile.ShallowWater();
+        mapArray[i] = shallowWater;
+    }
+    private void setBeach(int i)
+    {
+        beach = new Tile.Beach();
+        mapArray[i] = beach;
+    }
+    private void set(int i)
+    {
+        plain = new Tile.Plain();
+        mapArray[i] = plain;
+    }
+}
+*/
