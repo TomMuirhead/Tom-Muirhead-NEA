63148850455fab31b69088cdf496e15823656917
TomMuirhead <60030844+TomMuirhead@users.noreply.github.com>
2021-03-17 20:06:57 +0000

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,41 +8,66 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace NEA_Game
+namespace Play_menu_validation_fail
 {
     public partial class Form1 : Form
     {
+        private bool validInput = false;
+        private Color invalidColor = Color.Red;
+
         public Form1()
         {
             InitializeComponent();
         }
-        
-        // When "New Game" button is clicked
-        private void NewGameButton_Click(object sender, EventArgs e)
+
+        private void PlayButton_Click(object sender, EventArgs e)
         {
-            // Hide the Main Menu Screen
-            // Create New Game Menu and Open
-            this.Hide();
-            Form form2 = new Form2();
-            form2.ShowDialog();
-        }
-        
-        // When "Settings" button is clicked
-        private void SettingsButton_Click(object sender, EventArgs e)
-        {
-            // Hide the Main Menu Screen
-            // Create Settings Menu and Open
-            this.Hide();
-            Form form3 = new Form3();
-            form3.ShowDialog();
-        }
-        
-        // When "Quit" button is clicked
-        private void QuitButton_Click(object sender, EventArgs e)
-        {
-            // Closes the program
-            Application.Exit();
+            if (validInput == true)
+            {
+                this.Hide();
+                Form form4 = new Form4(textBox1.Text, textBox2.Text);
+                form4.ShowDialog();
+            }
+            else
+            {
+                label3.Visible = true;
+            }
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            validInput = CheckInput(textBox1);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            validInput = CheckInput(textBox2);
+        }
+
+        private bool CheckInput(TextBox textbox)
+        {
+            bool validInput = false;
+            textbox.BackColor = default(Color);
+            if (textbox.Text != "")
+            {
+                int InputNum;
+                bool validNum = Int32.TryParse(textbox.Text, out InputNum);
+                if (!validNum)
+                {
+                    textbox.BackColor = invalidColor;
+                    validInput = false;
+                }
+                else if (InputNum < 10 || InputNum > 100)
+                {
+                    textbox.BackColor = invalidColor;
+                    validInput = false;
+                }
+                else
+                {
+                    validInput = true;
+                }
+            }
+            return validInput;
+        }
     }
 }
